b6be6d972b6ce38ac08c21c931f60f45e6b1fb16
pil0706 <pil0706@gmail.com>
2017-04-24 16:04:39 +0900

    Tweet_crawler_streaming_encoding 수정

--- Tweet_crawler_Streaming_encoding.py
+++ Tweet_crawler_Streaming_encoding.py
@@ -4,7 +4,6 @@ import sys
 import jsonpickle
 import tweepy
 import json
-import codecs
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
@@ -16,25 +15,14 @@ consumer_secret = 'eeJckTn1C5yq6vCFxMoBbkjDOgh9ZMhQJtOpI5ogZgLfOuALGu'
 access_token = '153467658-vYrO9QedH95zM92FA2VzrTVQ54QzKsA2w2GgH6Ph'
 access_token_secret = 'qeJXZvay5uhKPS9E5aM2nMXqPBYxTyLDzYNk6X5qeutMt'
 
-#이부분은 인코딩때문에 해봄.
-import pprint
-class MyPrettyPrinter(pprint.PrettyPrinter):
-    def format(self, _object, context, maxlevels, level):
-        if isinstance(_object, unicode):
-            return "'%s'" % _object.encode('utf8'), True, False
-        elif isinstance(_object, str):
-            _object = unicode(_object, 'utf8')
-            return "'%s'" % _object.encode('utf8'), True, False
-        return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        #여기 인코딩부분 고친걸로 함 테스트
-        MyPrettyPrinter().pprint(data)
-        print MyPrettyPrinter().pformat(data)
+        json_encoded = json.dumps(data)
+        print json_encoded
 
         #json 저장하는거 (multi line으로 나옴. multi line json to single line 으로 나중에 변환 필요)
-        with open('Crawled_Data.json', 'a') as tf:
+        with open('tweet_data.json', 'a') as tf:
             tf.write(data)
         return True
 
@@ -54,4 +42,4 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords
-    stream.filter(track=[u'문재인', u'심상정', u'안철수', u'홍준표', u'유승민'])
+    stream.filter(track=[u'문재인', u'심상정', u'안철수',u'홍준표',u'유승민'])
